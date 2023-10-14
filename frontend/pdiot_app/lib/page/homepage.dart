4f3a34b54043a4edf33d760932d297830e8168b4
Henry der mba <zhonghe.zhang@hotmail.com>
2023-10-14 16:34:01 +0100

    update initial

--- frontend/pdiot_app/lib/page/homepage.dart
+++ frontend/pdiot_app/lib/page/homepage.dart
@@ -1,1 +1,36 @@
+import 'package:flutter/material.dart';
+import 'homepage_controller.dart'; // Import your controller
 
+class MyPage extends StatefulWidget {
+  @override
+  _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  // Instantiate the controller
+  MyPageController _controller = MyPageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("My Page")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Text(_controller.data), // Use controller's data
+            ElevatedButton(
+              onPressed: () {
+                // Use controller's logic, and refresh UI
+                setState(() {
+                  _controller.fetchData();
+                });
+              },
+              child: Text("Fetch Data"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ frontend/pdiot_app/lib/page/homepage_controller.dart
@@ -0,0 +1,11 @@
+class MyPageController {
+  // Variables, and methods to manage the page's data and logic
+
+  String data = "Initial Data";
+
+  void fetchData() {
+    // Imagine this function fetches/updates data, then updates `data`.
+    data = "Fetched Data";
+  }
+}
+// TODO Implement this library.
