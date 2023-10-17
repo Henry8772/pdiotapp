d0294470f05c09a039cdab7292527b0f14729d33
FMorgan-111 <maogenfu0121@163.com>
2023-10-17 16:24:13 +0100

    UI

--- frontend/pdiot_app/lib/main.dart
+++ frontend/pdiot_app/lib/main.dart
@@ -12,20 +12,61 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+        title: const Text("Flutter Demo"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Button 1'),
+              ),
+              SizedBox(height: 20), // spacing between buttons
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Button 2'),
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Button 3'),
+              ),
+            ],
+          ),
+        ),
+      
+
+        
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              label: "Home",
+              icon: Icon(Icons.home)
+              ),
+            BottomNavigationBarItem(
+              label: 'Settings',
+              icon:  Icon(Icons.settings)
+            )
+            ]
+      // theme: ThemeData(
+      //   // This is the theme of your application.
+      //   //
+      //   // Try running your application with "flutter run". You'll see the
+      //   // application has a blue toolbar. Then, without quitting the app, try
+      //   // changing the primarySwatch below to Colors.green and then invoke
+      //   // "hot reload" (press "r" in the console where you ran "flutter run",
+      //   // or simply save your changes to "hot reload" in a Flutter IDE).
+      //   // Notice that the counter didn't reset back to zero; the application
+      //   // is not restarted.
+      //   primarySwatch: Colors.blue,
+      // ),
+      // home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        )
+      )
     );
   }
 }
